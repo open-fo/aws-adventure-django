c6533db6ac3c017b66da19b7dac049cdb00d483b
cbinckly <cbinckly@gmail.com>
2019-01-29 15:46:49 +0100

    add the PostViewSet to retrieve and list posts.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from posts.models import Post
+from posts.serializers import PostSerializer
 
 # Create your views here.
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
