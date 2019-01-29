f2371cff78e97eff2a7b0805aa76842a6dd0a723
cbinckly <cbinckly@gmail.com>
2019-01-29 13:44:20 +0100

    defined the posts model and created migration.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+
+    class Meta:
+        pass
+
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    posted_on = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
