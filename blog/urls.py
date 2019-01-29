73449f3fad5ca7e7241a6bcd0c982ce088537d3a
cbinckly <cbinckly@gmail.com>
2019-01-29 15:47:42 +0100

    add a posts views to router and connect to /api/.

--- blog/urls.py
+++ blog/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from posts import views as post_views
+
+# Router for all API views
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('posts', post_views.PostViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
